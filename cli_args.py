a1fc09ba47a40df447f9783c44acb74bb4d65a48
Lukas Fürderer <l.fuerderer@gmail.com>
2021-05-10 15:35:21 +0200

    Create a first draft to parse command line arguments

---
+++ cli_args.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog='lssh')
+
+parser.add_argument('-r', '--replay', action='store_true', help='Watch a previously recored ssh session.')
+parser.add_argument('--timestamp', dest='TIME', help='The exact timestamp when the session recording was started, in the form YYYY-MM-DD_hh-mm-ss. (Implies --replay)')
+parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode. Enablkes printing the resulting ssh command line before connecting.')
+
+parser.add_argument('substring', metavar='[user@]substring', nargs='?', help='(Part of) the hostname to connect to, with an additional username if needed.')
+
+parser.parse_args()
