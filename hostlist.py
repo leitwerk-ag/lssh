5d00a0720d55cd2f9be6584899d92f397a2030de
Lukas Fürderer <l.fuerderer@gmail.com>
2021-05-12 13:54:34 +0200

    Allow the user to select hosts via tui

--- hostlist.py
+++ hostlist.py
@@ -1,21 +1,22 @@
 import os, re
 
 class HostEntry:
-    def __init__(self, displayname):
-        self.__displayname = displayname
-        self.__keywords = set()
+    def __init__(self, display_name, customer):
+        self.display_name = display_name
+        self.customer = customer
+        self.keywords = set()
 
 def load_config(path):
     entries = {}
     cur_host = None
-    def handle_line(line):
+    def handle_line(line, customer):
         m = re.match('^\s*Host\s+([\S]+)\s*$', line)
         if m:
             hostname = m.group(1)
             if '*' not in hostname and '?' not in hostname:
                 cur_host = hostname
                 if hostname not in entries:
-                    entries[hostname] = HostEntry(hostname)
+                    entries[hostname] = HostEntry(hostname, customer)
     files = os.listdir(path)
     files.sort()
     for filename in files:
@@ -23,5 +24,5 @@ def load_config(path):
         if filename.endswith(".txt") and os.path.isfile(name):
             with open(name, "r") as f:
                 for line in f:
-                    handle_line(line)
+                    handle_line(line, filename[0:-4])
     return entries
