4bc25ddfbbd84b9eee04cad57a7272700d75ffe1
Lukas Fürderer <l.fuerderer@gmail.com>
2021-05-12 14:48:11 +0200

    Fix a typo in the help text of the --verbose command

--- cli_args.py
+++ cli_args.py
@@ -5,7 +5,7 @@ def parse_args():
 
     parser.add_argument('-r', '--replay', action='store_true', help='Watch a previously recored ssh session.')
     parser.add_argument('--timestamp', metavar='TIME', dest='time', help='The exact timestamp when the session recording was started, in the form YYYY-MM-DD_hh-mm-ss. (Implies --replay)')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode. Enablkes printing the resulting ssh command line before connecting.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode. Enables printing the resulting ssh command line before connecting.')
 
     parser.add_argument('substring', metavar='[user@]substring', nargs='?', help='(Part of) the hostname to connect to, with an additional username if needed.')
 
